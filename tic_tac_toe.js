7ed4a13897a436ffcc57e2c3f544b126ae9a9a0c
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-05-28 22:03:53 -0700

    fix bug counting three empty cells as a match re-name allTriplets to allSets for readability ignore win check when < 5 turns in more explanatory comments

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -35,27 +35,35 @@ function ticTacToe () {
         board[y][x] = currentPlayer; 
         turns++;
         currentPlayer = currentPlayer === 1 ? 2 : 1;
-        return updateGameStatus();
+        return updateGameState();
     };
     /* checks state of game and returns new state */
-    const updateGameStatus = () => {
+    const updateGameState = () => {
+        // can't win before turn 5
+        if(turns < 5) {
+            return gameStatus = 0;
+        }
+        // if 9 turns then tie
         if(turns >= 9) { 
             return gameStatus = -1;
         }
         // start with all the columns
-        let allTriplets = Array.from(board);
+        let allSets = Array.from(board);
         // add all the rows
         for(let y = 0; y <= 2; y++) {
-            allTriplets.push([board[0][y],board[1][y],board[2][y]]);
+            allSets.push([board[0][y],board[1][y],board[2][y]]);
         }
         // add the two diagonals 
-        allTriplets.push([board[0][0],board[1][1],board[2][2]]);
-        allTriplets.push([board[2][0],board[1][1],board[0][2]]);
+        allSets.push([board[0][0],board[1][1],board[2][2]]);
+        allSets.push([board[2][0],board[1][1],board[0][2]]);
         // check them all
-        for(const i in allTriplets) {
-            if(allTriplets[i][0] === allTriplets[i][1] && 
-               allTriplets[i][0] === allTriplets[i][2]) {
-                return gameStatus = allTriplets[i][0];
+        for(let i = 0; i < allSets.length; i++) {
+            // ignore set if it starts with 0
+            if(!allSets[i][0]) { continue; }
+            // check if set matches
+            if(allSets[i][0] === allSets[i][1] && 
+               allSets[i][0] === allSets[i][2]) {
+                return gameStatus = allSets[i][0];
             }
         }
         return gameStatus = 0;
