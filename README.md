f7848516ebdbfca4841b24939ee5359ba61c505d
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-05-28 23:32:09 -0700

    fixed some bugs and added some functions added final thoughts to README

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 Odin Project assignment to make tic-tac-toe using what I've learned about factory functions and the module pattern.
 
 https://robotkio.github.io/odin-tic-tac-toe/
+
+It doesn't currently:
+- keep score
+- announce winner
+- display winning set
+
+But I'm feeling pretty comfortabale with where it's at. I think I can move on or look at other peoples versions, now.
+
+General thoughts:
+- I think I went a little overboard in scope without having the experience to back it up so this took a lot longer than I would have liked.
+- That said, I'm still happy with what I have and, because I did have things in smaller functions, changes as I went along became surprisingly straightforward.
+- I can't help but think I managed this in a way that's more complicated than is necessary.
+- I really only encapsulated the core game functions away from the display functions. The display functions still feel like they're probably too coupled. Heck, the core game rules may be too coupled. It just seemed like the rules were so small I didn't need, say, something like a whole object for a player. That may have changed if I was going to keep score.
+- I regret using svg's grabbed from https://pictogrammers.com/library/mdi/. It's not that they're not cool, and I probably could have just used them as images and saved myself time, I just like the idea of it rendering on the page. It also seems a lot easier to manipulate with CSS in that I can, say, set its colour with the "fill" attribute rather than modifying it with a CSS filter.
+- I vaguely planned the whole thing with the intent that I could have more than one game of tic-tac-toe going on. If you give t3Displayer the ID of another containing element the intent was that it would only query elements from within its own containers ID. Each displayer instance will also have its own, internal, tic-tac-toe game instance. I have no idea if that works, I have lost almost all interest in trying to make that a reality.
+- I'm very much looking forward to seeing what other people have done.
--- tic_tac_toe_display.js
+++ tic_tac_toe_display.js
@@ -1,53 +1,84 @@
 function t3Displayer (displayID) {
     // constant values
-    const player1SvcData = ["alpha-x-box-outline","M9,7H11L12,9.5L13,7H15L13,12L15,17H13L12,14.5L11,17H9L11,12L9,7M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M5,5V19H19V5H5Z"];
-    const player2SvcData = ["alpha-o-box-outline","M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M5,5V19H19V5H5M11,7H13A2,2 0 0,1 15,9V15A2,2 0 0,1 13,17H11A2,2 0 0,1 9,15V9A2,2 0 0,1 11,7M11,9V15H13V9H11Z"];
-    const blankSpaceSvcData = ["checkbox-blank-outline","M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"];
+    const player1SvgData = ["alpha-x-box-outline","M9,7H11L12,9.5L13,7H15L13,12L15,17H13L12,14.5L11,17H9L11,12L9,7M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M5,5V19H19V5H5Z"];
+    const player2SvgData = ["alpha-o-box-outline","M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M5,5V19H19V5H5M11,7H13A2,2 0 0,1 15,9V15A2,2 0 0,1 13,17H11A2,2 0 0,1 9,15V9A2,2 0 0,1 11,7M11,9V15H13V9H11Z"];
+    const blankSpaceSvgData = ["checkbox-blank-outline","M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"];
     const player1Class = "player-1";
     const player2Class = "player-2";
     const inactivePlayerClass = "inactive-player";
     const tieClass = "tie";
-    const id = displayID;
+    const cellSelector = `#${displayID} .cell`;
+    const resetButtonText_reset = "Reset Game";
+    const resetButtonText_replay = "Play Again";
     // element queries
-    const boardContainer = document.querySelector(`#${id} .board-container`);
-    const p1Tab = document.querySelector(`#${id} .player-tab.player-1`);
-    const p2Tab = document.querySelector(`#${id} .player-tab.player-2`);
-    const game = ticTacToe();
-    const resetButton = document.querySelector(`#${id} .reset-button`);
-    const dispBoard = document.querySelector(`#${id} .board`);
+    const boardContainer = document.querySelector(`#${displayID} .board-container`);
+    const p1Tab = document.querySelector(`#${displayID} .player-tab.player-1`);
+    const p2Tab = document.querySelector(`#${displayID} .player-tab.player-2`);
+    const resetButton = document.querySelector(`#${displayID} .reset-button`);
+    const displayBoard = document.querySelector(`#${displayID} .board`);
     // setup
+    const game = ticTacToe();
     resetButton.addEventListener("click", () => {
         newGame();
     });
     // methods
-    const _makeSvc = (t, d) => {
-        const svc = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svc.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svc.setAttribute("viewBox", "0 0 24 24");
-        const title = document.createElement("title");
-        title.innerText = t;
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", d);
-        svc.appendChild(title);
-        svc.appendChild(path);
-        return svc;
-    };
     const newGame = () => {
         game.newGame();
         _render();
         _addClickFunctions();
+        resetButton.innerText = resetButtonText_reset;
     };
-    const _render = () => {
+    const play = (x, y) => {
+        const gameState = game.play(x, y);
+        switch (gameState) {
+            case 1: // p1 won
+                _renderBoard();
+                resetButton.innerText = resetButtonText_replay;
+                break;
+            case 2: // p2 won
+                resetButton.innerText = resetButtonText_replay;
+                _renderBoard();
+                break;
+            case -1: // tie
+                _renderBoard();
+                p1Tab.classList.add(inactivePlayerClass);
+                p2Tab.classList.add(inactivePlayerClass);
+                boardContainer.classList.remove(player1Class);
+                boardContainer.classList.remove(player2Class);
+                boardContainer.classList.add(tieClass);
+                break;
+            case 0:
+                _render();
+                _addClickFunctions();
+                break;
+            default:
+                _render();
+                _addClickFunctions();
+                console.warn("invalid switch case in play()");
+        }
+    };
+    // internal functions
+    function _makeSvg(t, d) {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        const title = document.createElement("title");
+        title.innerText = t;
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", d);
+        svg.appendChild(title);
+        svg.appendChild(path);
+        return svg;
+    }
+    function _render() {
         _renderTabs();
         _renderBoard();
-    };
-    const _clearBoard = () => {
-        const cells = document.querySelectorAll(`#${id} .cell`);
-        cells.forEach( (cell) => {
-            cell.remove();
-        });
-    };
-    const _renderTabs = () => {
+    }
+    function _clearBoard() {
+        const cells = document.querySelectorAll(cellSelector);
+        cells.forEach( cell => cell.remove() );
+    }
+    function _renderTabs() {
         switch(game.whosTurn()) {
             case 1:
                 boardContainer.classList.add(player1Class);
@@ -62,10 +93,10 @@ function t3Displayer (displayID) {
                 p2Tab.classList.remove(inactivePlayerClass);
                 break;
             default:
-                console.log("invalid switch case in _renderTabs()");
+                console.error("invalid switch case in _renderTabs()");
         }
-    };
-    const _renderBoard = () => {
+    }
+    function _renderBoard() {
         _clearBoard();
         const localBoard = game.getBoard();
         for(let y = 0; y < localBoard.length; y++){
@@ -73,25 +104,31 @@ function t3Displayer (displayID) {
                 const div = document.createElement("div");
                 div.classList.add("cell");
                 div.classList.add(`${x}-${y}`);
+                let svgTitle, svgPath;
                 switch (localBoard[y][x]) {
                     case 0:
-                        div.appendChild(_makeSvc(blankSpaceSvcData[0],blankSpaceSvcData[1]));
+                        svgTitle = blankSpaceSvgData[0];
+                        svgPath = blankSpaceSvgData[1];
                         break;
                     case 1:
-                        div.appendChild(_makeSvc(player1SvcData[0],player1SvcData[1]));
+                        svgTitle = player1SvgData[0];
+                        svgPath = player1SvgData[1];
                         break;
                     case 2:
-                        div.appendChild(_makeSvc(player2SvcData[0],player2SvcData[1]));
+                        svgTitle = player2SvgData[0];
+                        svgPath = player2SvgData[1];
                         break;
                     default:
-                        console.log("invalid switch case in _renderBoard()");
+                        console.error("invalid switch case in _renderBoard()");
                 }
-                dispBoard.appendChild(div);
+                div.appendChild(_makeSvg(svgTitle, svgPath));
+                displayBoard.appendChild(div);
             }
+            
         }
-    };
-    const _addClickFunctions = () => {
-        const cells = document.querySelectorAll(`#${id} .cell`);
+    }
+    function _addClickFunctions() {
+        const cells = document.querySelectorAll(cellSelector);
         cells.forEach( (cell) => { 
             cell.addEventListener("click", (e) => {
                 const coords = e.target.classList[1].toString();
@@ -100,30 +137,8 @@ function t3Displayer (displayID) {
                 play(x, y);
             })
         });
-    };
-    const play = (x, y) => {
-        const gameState = game.play(x, y);
-        switch (gameState) {
-            case 1: // p1 won
-                _renderBoard();
-                break;
-            case 2: // p2 won
-                _renderBoard();
-                break;
-            case -1: // tie
-                _renderBoard();
-                p1Tab.classList.add(inactivePlayerClass);
-                p2Tab.classList.add(inactivePlayerClass);
-                boardContainer.classList.remove(player1Class);
-                boardContainer.classList.remove(player2Class);
-                boardContainer.classList.add(tieClass);
-                break;
-            case 0:
-            default:
-                _render();
-                _addClickFunctions();
-        }
-    };
+    }
+    // initialization
     _render();
     _addClickFunctions();
     return { play, newGame };
